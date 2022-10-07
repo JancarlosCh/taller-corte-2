135e534004ee2608ef01d10b18bf0eed09006230
Jan Charris <jancarlosch@outlook.es>
2022-10-07 01:39:20 -0500

    updated registration form

--- resources/views/auth/signup/index.blade.php
+++ resources/views/auth/signup/index.blade.php
@@ -24,40 +24,42 @@
 
 @section('content')
     <div class="container-fluid">
-        <form action="{{ route('signup.store') }}" method="POST">
-            @csrf
-            <h1 class="text-center mt-5">Formulario de registro</h1>
-            <div class="mb-3">
-                <label for="name" class="form-label">Nombre</label>
-                <input type="text" name="name" id="name" class="form-control">
-            @error('name')
-                <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
-            @enderror
-            </div>
-            <input type="hidden" name="is_admin" value="0">
-            <div class="mb-3">
-                <label for="email" class="form-label">Correo Electrónico</label>
-                <input type="email" name="email" id="email" class="form-control">
-            @error('email')
-                <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
-            @enderror
-            </div>
-            <div class="mb-3">
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" id="password" class="form-control">
-            @error('password')
-                <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
-            @enderror
-            </div>
-            <div class="d-grid">
-                <button type="submit" class="btn btn-primary">Registrarse</button>
-            </div>
-        </form>
-        <div class="d-grid mt-2">
-            <a href="{{ route('welcome') }}" class="btn btn-danger">Cancelar</a>
-        </div>
-        <div class="d-flex justify-content-center mt-3">
-            <a href="{{ route('login.show') }}">¿Ya tienes una cuenta? Iniciar Sesión</a>
+        <div class="row justify-content-center">
+            <form action="{{ route('signup.store') }}" method="POST" class="col-sm-9 col-md-9 col-lg-9 col-xl-6">
+                @csrf
+                <h1 class="text-center mt-5">Formulario de registro</h1>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nombre</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                @error('name')
+                    <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
+                @enderror
+                </div>
+                <input type="hidden" name="is_admin" value="0">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Correo Electrónico</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                @error('email')
+                    <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
+                @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                @error('password')
+                    <p class="form-text text-danger mb-0 p-0">{{ $message }}</p>
+                @enderror
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Registrarse</button>
+                </div>
+                <div class="d-grid mt-2">
+                    <a href="{{ route('welcome') }}" class="btn btn-danger">Cancelar</a>
+                </div>
+                <div class="d-flex justify-content-center mt-3">
+                    <a href="{{ route('login.show') }}">¿Ya tienes una cuenta? Iniciar Sesión</a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
